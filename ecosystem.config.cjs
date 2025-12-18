9c51ada9c8b7ea2f28512db385cf19a31c3d25ff
alt45 <alto45x@gmail.com>
2025-12-18 23:59:45 +0700

    update  pm2

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -7,6 +7,9 @@ module.exports = {
       autorestart: true,
       watch: false,
       max_memory_restart: '1G',
+      out_file: './logs/out.log',
+      error_file: './logs/error.log',
+      log_date_format: 'YYYY-MM-DD HH:mm:ss',
       env: {
         NODE_ENV: 'development',
       },
